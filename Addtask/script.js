2fda1536cbe23aa6b3ea043cb518329f7da64bb5
Sam-Codes-06 <smartsamyakmarathe@gmail.com>
2025-02-13 12:04:09 +0530

    Now able to add elements in the to-do-list

--- Addtask/script.js
+++ Addtask/script.js
@@ -5,13 +5,16 @@ let taskList = document.getElementById("taskList");
 
 // Function to add a task
 function addTask() {
-
+    let tasks = taskInput.value.trim();
+    
     // take task text
-
+    let li = document.createElement("li");
+    li.textContent = tasks;
     // Append task
-
+    taskList.appendChild(li);
     // Clear input
+    taskInput.value = "";
 }
 
 // Event Listener
-// addTaskButton.addEventListener("click", addTask);
+addTaskButton.addEventListener("click", addTask);
